81c7dc1b8a86f532556cb550b92b37e6b130dde9
Mark Standen <no-reply@rundog.dev>
2024-06-01 17:28:52 +0100

    Updated homepage with eventListener

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import './tailwind/tailwind.css';
 import { tw } from './tailwind/tw/tw.ts';
 
-const app = document.getElementById('app')!;
+addEventListener('DOMContentLoaded', async () => {
+    const app = document.getElementById('app')!;
 
-app.innerHTML = `
+    app.innerHTML = `
       <div class="${tw`w-20 border-2 border-red-700 px-2 py-1 text-center text-red-700`}">
         Test
       </div>
 `;
 
-const res = await fetch('/full');
-const json = await res.json();
-console.log(json);
+    const res = await fetch('/full');
+    const json = await res.json();
+    console.log(json);
+});
