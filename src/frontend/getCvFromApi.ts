889f126e70b68d7a873db7938d7eb0328784ca91
Mark Standen <no-reply@rundog.dev>
2024-06-09 09:05:29 +0100

    wrapped function pre-application in another function to remove top level await.

--- src/frontend/getCvFromApi.ts
+++ src/frontend/getCvFromApi.ts
@@ -6,5 +6,7 @@ import { API_BASE_PATH } from '../constants.ts';
  * Returns a preconfigured function to retrieve CV data from the API
  * @type {(id?: string) => Promise<CV>}
  */
-export const getCvFromApi: (id?: string) => Promise<CV> =
-    await getFromApiFactory<CV>(fetch, API_BASE_PATH);
+export async function getCvFromApi(id?: string): Promise<CV> {
+    const _getCvFromApi = await getFromApiFactory<CV>(fetch, API_BASE_PATH);
+    return _getCvFromApi(id);
+}
